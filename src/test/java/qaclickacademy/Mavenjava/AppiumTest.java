a65c905396795db8cacb4a4e83921ed7763786fa
sushmitha <sush10610@gmail.com>
2021-09-20 12:15:04 -0400

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+@Test
+public void IOS()
+
+{
+	System.out.println("IOS");
+}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/seleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsIo()
+	{
+		System.out.println("elementsIo");
+	}
+}
+
